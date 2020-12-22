90b63c1fed593f309a38e8fa1a917cfb1e9a4b88
Arindam Dutta <66781582+arindam1903@users.noreply.github.com>
2020-12-23 04:48:05 +0530

    assignment

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-
-function handleServer(req, res) {
-  
+const handleServer=(req, res) =>{
+  if (req.url === "/welcome") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Welcome to Dominos!");
+    res.end();
+  } else if (req.url === "/contact") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(
+      JSON.stringify({
+        phone: "18602100000",
+        email: "guestcaredominos@jublfood.com"
+      })
+    );
+    res.end();
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("404 Not Found");
+  }
 }
-
+httpServer.listen(8081);
 module.exports = httpServer;
