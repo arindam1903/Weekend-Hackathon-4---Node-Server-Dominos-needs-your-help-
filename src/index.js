3dcf89bfc7003a03a85300c37e74667383d8f23b
Arindam Dutta <66781582+arindam1903@users.noreply.github.com>
2021-02-07 20:47:06 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const http=require("http");
 
 const server=http.createServer((req,res)=>{
   if (req.url==='/welcome'){
-      res.writeHead(200);
+      res.writeHead(200,{"Content-Type":"application/json"});
     res.write(('Welcome to Dominos!'));
     res.end();
     
