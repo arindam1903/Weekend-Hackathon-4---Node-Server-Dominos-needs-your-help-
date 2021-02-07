426b208820ff60fafeb5525790c7e5270211df62
Arindam Dutta <66781582+arindam1903@users.noreply.github.com>
2021-02-07 21:32:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var http = require("http");
 const httpServer = http.createServer(handleServer);
 function handleServer(req, res) {
   if (req.url === "/welcome") {
-    res.writeHead(200);
+    res.writeHead(200,{ "Content-Type": "text/plain" });
     res.write("Welcome to Dominos!");
     res.end();
   } else if (req.url === "/contact") {
