c51dfe796b4c65d4c9032d593f11c4173b2193d8
Arindam Dutta <66781582+arindam1903@users.noreply.github.com>
2021-02-07 20:56:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-const http=require("http");
+var http = require("http");
 
-const server=http.createServer((req,res)=>{
-  if (req.url==='/welcome'){
-      res.writeHead(200,{"Content-Type":"text:plain"});
-    res.write(('Welcome to Dominos!'));
+const httpServer = http.createServer(handleServer);
+function handleServer(req, res) {
+  if (req.url === "/welcome") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Welcome to Dominos!");
     res.end();
-    
-  }
-  else if (req.url==='/contact'){
-    res.write(JSON.stringify({
-      phone: '18602100000',
-      email: 'guestcaredominos@jublfood.com'
-    }));
+  } else if (req.url === "/contact") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.write(
+      JSON.stringify({
+        phone: "18602100000",
+        email: "guestcaredominos@jublfood.com"
+      })
+    );
     res.end();
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("404 Not Found");
   }
-});
-server.listen(8081);
+}
+httpServer.listen(8081);
+module.exports = httpServer;
