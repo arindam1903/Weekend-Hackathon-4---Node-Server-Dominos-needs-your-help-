9b5d885f9af5a58416a1b1812959392330a8b2fa
Arindam Dutta <66781582+arindam1903@users.noreply.github.com>
2021-02-07 21:02:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ var http = require("http");
 const httpServer = http.createServer(handleServer);
 function handleServer(req, res) {
   if (req.url === "/welcome") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.writeHead(200);
     res.write("Welcome to Dominos!");
     res.end();
   } else if (req.url === "/contact") {
-    res.writeHead(200, { "Content-Type": "application/json" });
+    res.writeHead(200);
     res.write(
       JSON.stringify({
         phone: "18602100000",
