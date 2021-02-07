d781c66d61bba329143036701be0daf95adcb66e
Arindam Dutta <66781582+arindam1903@users.noreply.github.com>
2021-02-07 21:04:04 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function handleServer(req, res) {
     res.write("Welcome to Dominos!");
     res.end();
   } else if (req.url === "/contact") {
-    res.writeHead(200);
+    res.writeHead(200,,{ "Content-Type": "application/json" });
     res.write(
       JSON.stringify({
         phone: "18602100000",
@@ -16,7 +16,7 @@ function handleServer(req, res) {
     );
     res.end();
   } else {
-    res.writeHead(404,{ "Content-Type": "application/json" });
+    res.writeHead(404);
     res.end("404 Not Found");
   }
 }
